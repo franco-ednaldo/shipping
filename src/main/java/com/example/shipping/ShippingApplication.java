b0012eb7b718725b60548317edc635752e1f5118
franco-ednaldo <ednaldofranco.dev@gmail.com>
2023-11-17 18:06:04 -0300

    implementado formatação

--- src/main/java/com/example/shipping/ShippingApplication.java
+++ src/main/java/com/example/shipping/ShippingApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.text.DecimalFormat;
+
 @SpringBootApplication
 public class ShippingApplication implements CommandLineRunner {
     private final OrderService orderService;
@@ -20,18 +22,19 @@ public class ShippingApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        final var formatter = new DecimalFormat("#.00");
         final var order1 = new Order(1034, 150.0, 20.0);
         System.out.println("Pedido código: " + order1.code());
-        System.out.println("Valor total: " + this.orderService.total(order1));
+        System.out.println("Valor total: " + formatter.format(this.orderService.total(order1)));
         System.out.println("--------------- -----------------");
 
         final var order2 = new Order(2282, 800.00, 10.0);
         System.out.println("Pedido código: " + order2.code());
-        System.out.println("Valor total: " + this.orderService.total(order2));
+        System.out.println("Valor total: " + formatter.format(this.orderService.total(order2)));
 
         System.out.println("--------------- -----------------");
         final var order3 = new Order(1309, 95.90, 0.0);
         System.out.println("Pedido código: " + order3.code());
-        System.out.println("Valor total: " + this.orderService.total(order3));
+        System.out.println("Valor total: " + formatter.format(this.orderService.total(order3)));
     }
 }
